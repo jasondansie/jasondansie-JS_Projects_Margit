df480ce094c61b3a01b3216b56be382543649bd3
jasondansie <jasondansie@gmail.com>
2022-08-17 11:20:51 +0300

    completed task 4

---
+++ week_2/task_4/main.js
@@ -0,0 +1,36 @@
+function getRandomNumbers()
+{
+    let num1, num2, num3;
+
+    num1 = promptForNumber("first");
+    num2 = promptForNumber("Second");
+    num3 = promptForNumber("Third");
+
+    num1 = parseInt(num1);
+    num2 = parseInt(num2);
+    num3 = parseInt(num3);
+
+    if (num1 < 0 && num2 < 0 && num3 < 0) {
+        console.log("Only Negatives");
+    }
+    else if(num1 > 0 && num2 > 0 && num3 > 0)
+    {
+        const sum = num1+num2+num3;
+        console.log("The sum of your numbers are: " + sum);
+        const multiplyNum = num1*num2*num3;
+        console.log("The multiplication of your numbers is: " + multiplyNum);
+    }
+    else
+    {
+        const sum = num1+num2+num3;
+        console.log("The sum of your numbers are: " + sum);
+    }
+}
+
+function promptForNumber(phrase)
+{
+    const num = window.prompt("Enter " + phrase + " number");
+    return num;
+}
+
+getRandomNumbers();
