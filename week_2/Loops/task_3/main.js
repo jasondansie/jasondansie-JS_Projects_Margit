5913cadd8d968658341edf7ffcc77ee93bf9baea
jasondansie <jasondansie@gmail.com>
2022-08-18 12:05:56 +0300

    made it a loop.

--- week_2/Loops/task_3/main.js
+++ week_2/Loops/task_3/main.js
@@ -1,17 +1,24 @@
 function avarageSpeed() 
 {
-    const distanceKm = parseFloat(prompt("Enter a distance in km?"));
-    const timeH = parseFloat(prompt("Enter the time in hours?"));
+    let stopper;
+ 
+    do {
+        const distanceKm = parseFloat(prompt("Enter a distance in km?"));
+               
+        if (distanceKm == 0) {
+            console.log("Program is exiting Good bye!");
+            stopper = distanceKm;
+            return 0;
+        }
+        else
+        { 
+            const timeH = parseFloat(prompt("Enter the time in hours?"));
 
-    if (distanceKm == 0) {
-        console.log("Program is exiting Good bye!");
-        return 0;
-    }
-    else
-    {
-        let avgSpeed = distanceKm/timeH;
-        console.log("The average speed is:", avgSpeed, "km.");
-    }    
+            let avgSpeed = distanceKm/timeH;  
+            console.log("The average speed is:", avgSpeed, "km.");
+        }
+
+    } while (stopper != 0); 
 }
 
 avarageSpeed();
