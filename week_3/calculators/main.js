e1584bd852b5fa8c80945ed2f9222b92c052413c
Jason Dansie <jasondansie@gmail.com>
2022-08-24 12:30:47 +0300

    adds code refactering for W3T1

--- week_3/calculators/main.js
+++ week_3/calculators/main.js
@@ -1,14 +1,17 @@
-function calculateGasPrice()
-{
-    const price = document.getElementById("priceInput").value;
-    const money = document.getElementById("moneyInput").value;
-    
-    let liters = money/price;
-    if (liters < 10) {
-        document.getElementById("gasPriceResults").textContent = `You could get about ${liters} litters, Ups, you have to stay here.`;
-    }
-    else
-    {
-        document.getElementById("gasPriceResults").textContent = `You could get about ${liters} litters, good, you can escape now.`;
-    }
+function calculateGasPrice() {
+  const price = document.getElementById("priceInput").value;
+  const money = document.querySelector("#moneyInput").value;
+
+  const answer = document.querySelector("#gasPriceResults");
+  const answerDiv = document.querySelector("#gasPriceDiv");
+
+  let liters = Math.floor(money / price);
+
+  if (liters < 10) {
+    answer.textContent = `You could get about ${liters} litters, Ups, you have to stay here.`;
+    answerDiv.style.backgroundColor = "red";
+  } else {
+    answer.textContent = `You could get about ${liters} litters, good, you can escape now.`;
+    answerDiv.style.backgroundColor = "green";
+  }
 }
