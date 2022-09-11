7a5ecd212e3228132ed5b2bf1549f47567d42cb0
jasondansie <jasondansie@gmail.com>
2022-09-11 22:25:07 +0300

    fixes search function

--- week_4/Car Database/main.js
+++ week_4/Car Database/main.js
@@ -7,7 +7,10 @@ const carPrice = document.querySelector("#car_price");
 const carColor = document.querySelector("#car_color");
 const addCar = document.querySelector("#add");
 const resetCarsArray = document.querySelector("#reset");
-const form = document.querySelector("form");
+const carSearchButton = document.querySelector("#carSearch");
+const searchBox = document.querySelector("#searchBox");
+const searchResults = document.querySelector("#searchResults");
+
 
 const CarsArray = [];
 
@@ -56,7 +59,7 @@ addRow = () => {
 
 addCar.addEventListener("click", addRow);
 
-resetCars = () =>{
+resetCars = () => {
     CarsArray.length = 0;
     while (CarsArray.length > 0) {
         CarsArray.pop();
@@ -64,8 +67,42 @@ resetCars = () =>{
     while (carTableBody.childNodes.length) {
         carTableBody.removeChild(carTableBody.childNodes[0]);
       }
+      
+      document.querySelector("#searchForm").reset(); 
+      document.querySelector("#carForm").reset(); 
 }
 
 resetCarsArray.addEventListener("click", resetCars);
 
+searchForCar = () => {
+    console.log("search:", searchBox.value);
+    console.log("carsArray: ", CarsArray);
+    let foundCar = [];
+
+    CarsArray.forEach(car => {
+        console.log(car);
+        if (car[0] == searchBox.value) {
+            foundCar = car;
+        }
+        else
+        {
+            foundCar == "";
+        }
+
+    });
+
+    if (foundCar != "") 
+    {
+        searchResults.textContent = `license number ${foundCar[0]} is a ${foundCar[1]} ${foundCar[2]} and it belongs to ${foundCar[3]} and it is ${foundCar[5]}.`;
+    }
+    else
+    {
+        searchResults.textContent = "No car found with that license number"
+    }
+
+    console.log("foundCar: ", foundCar);
+    
+}
+
+carSearch.addEventListener("click", searchForCar);
 addRow();
