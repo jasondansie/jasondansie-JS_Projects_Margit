d0f16c2d7dbb1613aacdd2007fc427edcd930b27
Jason Dansie <jasondansie@gmail.com>
2022-09-13 12:14:06 +0300

    adds code cleanup and refactoring.

--- week_4/BG Gradiant/main.js
+++ week_4/BG Gradiant/main.js
@@ -1,13 +1,13 @@
 const radioButton = document.querySelectorAll(`input[name="direction"]`);
 const colorButton1 = document.querySelector("#color1");
 const colorButton2 = document.querySelector("#color2");
-const gradientArea = document.querySelector("section");
+const gradientArea = document.querySelector("section").style.backgroundImage;
 
 let direction = " to bottom";
 let color1 = "red";
 let color2 = "orange";
 
-const cssText = document.querySelector("#cssText");
+const cssText = document.querySelector("#cssText").textContent;
  
 changeColor = () => {
     if (color1 != "undefined") {
@@ -16,27 +16,19 @@ changeColor = () => {
     if (color2 != "undefined"){
         color2 = colorButton2.value;
     }  
-    changeGradient();
+    gradientArea= `linear-gradient(${direction}, ${color2}, ${color1})`;
+    cssText = `CSS: linear-gradient(${direction}, ${color2}, ${color1})`;
 }
 
 radioButton.forEach((button) => {
     button.addEventListener("change", function() {
         if (button.id != "middle") {
             direction = `to ${button.id}`;
-            changeGradient();
+            gradientArea= `linear-gradient(${direction}, ${color2}, ${color1})`;
+            cssText = `CSS: linear-gradient(${direction}, ${color2}, ${color1})`;
         }     
       });
 });
-
-changeGradient = () => {
-    gradientArea.style.backgroundImage = `linear-gradient(${direction}, ${color2}, ${color1})`;
-    changeCSSText();
-}
-
-changeCSSText = () => {
-    cssText.textContent = `CSS: linear-gradient(${direction}, ${color2}, ${color1})`;
-}
-
 
 colorButton1.addEventListener("change", changeColor);
 colorButton2.addEventListener("change", changeColor);
