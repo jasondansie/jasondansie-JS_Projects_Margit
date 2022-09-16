cde22c4b5064a2fd30f2cce4f3fba76a0ede3611
Jason Dansie <jasondansie@gmail.com>
2022-09-16 11:59:44 +0300

    adds code refactoring

--- week_5/Dom/main.js
+++ week_5/Dom/main.js
@@ -1,14 +1,56 @@
 
 const header = document.querySelector("header");
+const scrollUpButton = document.querySelector("#scrollUpButton");
+const modalButton = document.querySelector("modalButton");
+const overlay = document.querySelector("overlay");
+const modal = document.querySelector("modal");
+const closeButton = document.querySelector("closeButton");
+const mobileButton = document.querySelector("#mobileButton");
+const nav = document.querySelector("nav");
 
 
-window.addEventListener('scroll', function() {
-    let scrollHeight = document.getElementById('showScroll').innerHTML = window.pageYOffset;
+window.onscroll = function() {scrollFunction()};
 
-    if (scrollHeight > 200) {
-        header.style.backgroundColor = "black";
+function scrollFunction() {
+  if (document.body.scrollTop > 200 || document.documentElement.scrollTop > 200) {
+    header.classList.add("bg");
+    scrollUpButton.style.display = "block";
+  } else {
+    header.classList.remove("bg");
+    scrollUpButton.style.display = "none";
+  }
+}
+
+function topFunction() {
+    document.body.scrollTop = 0;
+    document.documentElement.scrollTop = 0;
+  }
+
+function openModal() {
+    overlay.style.display = "block";
+    modal.style.display = "block";
+    closeButton.style.display = "block";
+}
+
+function closeModal() {
+    overlay.style.display = "none";
+    modal.style.display = "none";
+    closeButton.style.display = "none";
+}
+
+
+
+const openMobileMenu = () =>{
+    console.log("button pressed");
+    if(nav.classList.contains("responsive")){
+        nav.classList.remove("responsive");
+        document.body.style.overflow = '';
     }
     else{
-        header.style.backgroundColor = "rgba(0, 0, 0, 0.4)";
+        nav.classList.add("responsive");
+        document.body.style.overflow = '';
     }
-  });
+}
+
+  scrollUpButton.addEventListener("click", topFunction);
+  mobileButton.addEventListener("click", openMobileMenu);
