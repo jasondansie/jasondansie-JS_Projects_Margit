48e868e2ed37997a42a546318bd37bea71d8c854
Jason Dansie <jasondansie@gmail.com>
2022-10-02 10:54:53 +0300

    add pokedex working with names

--- Week_8/pokedex/main.js
+++ Week_8/pokedex/main.js
@@ -1,19 +1,29 @@
 
 const cards = document.querySelector(".cards");
 let pokedex = [];
+let pokemans = [];
 
 
 const getPokeDex =  () => {
-    fetch("https://pokeapi.co/api/v2/pokemon?offset=0&limit=").then((response) => response.json())
+    fetch("https://pokeapi.co/api/v2/pokemon?offset=0&limit=10").then((response) => response.json())
     .then((data) => {
         pokedex = data.results;
         pokeCard();
     });
 }
 
-getPokeDex();
+const getPokemanData = () => {
+    const card = pokedex.map((pokeman) => {
+        fetch(pokeman.url).then((response) => response.json())
+    .then((data) => {
+         pokemans.push(data);
+        });             
+    })   
+    pokeCard();
+}
 
 const pokeCard = () => {
+    
     const card = pokedex.map((pokeman) => {
         return `<div class = "card">
         <img src="https://images.secretlab.co/theme/common/collab_pokemon_catalog_charizard-min.png" alt="random image">
@@ -21,7 +31,11 @@ const pokeCard = () => {
             <h1>${pokeman.name}</h1>
             <i class="fa-brands fa-css3"></i>
         </div>  
-        </div>               `
+        </div>`
     }).join('');
+    
     cards.innerHTML = card;
+
 }
+
+getPokeDex();
